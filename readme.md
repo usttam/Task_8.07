d0347db67aa6095014848907b00b9576d722199f
usttam <tamulinas.ustinas@hotmail.com>
2022-12-18 16:57:49 +0300

    add readme file

---
+++ readme.md
@@ -0,0 +1,20 @@
+# A game in which the computer guesses the number conceived by the user.
+
+Developed as a practical task on the course '_Full-stack developer'_
+---
+
+## Technologies used:
+
+* HTML
+
+* CSS it
+
+* Visual Studio Code
+
+* JavaScript
+
+## How to open/run
+
+Go to the site folder in the file manager, double-click on the ___index.html___ file.
+
+
--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let answerNumber  = 0;
 let orderNumber = 0;
 let gameRun = false;
 
+
 const orderNumberField = document.querySelector('#orderNumberField');
 const answerField = document.querySelector('#answerField');
 
@@ -43,19 +44,21 @@ document.querySelector('#btnLess').addEventListener('click',()=>{
 
 function GameStart (){
     minValue = parseInt(prompt('The minimum value of the number for the game','0')) || 0; 
-    (minValue < -999) ?  minValue = -999 :  minValue = minValue;
+    (minValue < -999) || (minValue > 998) ?  minValue = -999 :  minValue = minValue;
     maxValue = parseInt(prompt('The maximum value of the number for the game','100')) || 100;
-    (maxValue > 999) ?  maxValue = 999 :  maxValue = maxValue;
+    (maxValue > 999) || (maxValue < -998) ?  maxValue = 999 :  maxValue = maxValue;  
     alert(`Think of a number from ${minValue} to ${maxValue}, and i guess it`);
     answerNumber  = Math.floor((minValue + maxValue) / 2);
     orderNumber = 1;
     gameRun = true;
     orderNumberField.innerText = orderNumber;
-    answerField.innerText = `You guessed the number is ${answerNumber }?`;
+    const answerText=TextAnswer (answerNumber);
+    answerField.innerText = `You guessed the number is ${answerNumber }? \n (${answerText})`;
 }
 
-function CheckValue (isLess){           
-    if (gameRun){
+function CheckValue (isLess){  
+           
+    if (gameRun && typeof(isLess)==='boolean'){
         let answerPhrase;
         if ((isLess && minValue === answerNumber) ||  minValue === maxValue){
             const phraseRandom = Math.round( Math.random()*2);            
@@ -72,25 +75,265 @@ function CheckValue (isLess){
             } 
             answerField.innerText = answerPhrase;
             gameRun = false;
-        } else {        
-            //debugger;
+        } else {            
             const phraseRandom = Math.round( Math.random()*2);                                
             (isLess)? maxValue = answerNumber  - 1 : minValue = answerNumber  + 1;
             answerNumber  = Math.floor((minValue + maxValue) / 2);
             orderNumber++;
             orderNumberField.innerText = orderNumber;
+            const answerText= TextAnswer (answerNumber);
             switch (phraseRandom) {
                 case 1:
-                    answerPhrase = `And this number is ${answerNumber }?`; 
+                    answerPhrase = `And this number is ${answerNumber }? \n(${answerText})`; 
                     break;
                 case 2:
-                    answerPhrase = `Yes, it's easy! You made a guess ${answerNumber }?`; 
+                    answerPhrase = `Yes, it's easy! You made a guess ${answerNumber }? \n(${answerText})`; 
                     break;
                 default: 
-                    answerPhrase = `You guessed the number ${answerNumber }?`;
+                    answerPhrase = `You guessed the number ${answerNumber }? \n(${answerText})`;
                     break;
             } 
             answerField.innerText = answerPhrase;
+            
         }
+    } else {
+        console.log ('gameRun is False or isLess is not boolean');
+    }
+}
+function TextAnswer (answerNumber){
+    let remOfNumber1,remOfNumber2;   
+    let hunrds,tens, ones;    
+    let isMinus
+    let textAnswer;
+    if (Number.isInteger(answerNumber)) {
+        if (answerNumber > 0) {                        
+            isMinus = false;                   
+        }
+        if (answerNumber <0 ){
+            answerNumber = answerNumber * (-1);
+            isMinus=true;
+        }
+        if (answerNumber == 0) {
+            return 'ноль';
+        }   
+         
+        remOfNumber1 = answerNumber % 100;
+        remOfNumber2 = answerNumber % 10;
+
+     // debugger;
+      
+
+        if (remOfNumber1 == 0){
+            switch (answerNumber) {
+                case 900:
+                    textAnswer = 'девятьсот';                        
+                    break;
+                case 800:
+                    textAnswer = 'восемьсот';                        
+                    break;
+                case 700:
+                    textAnswer = 'семьсот';                        
+                    break;
+                case 600:
+                    textAnswer = 'шестьсот';                        
+                    break;
+                case 500:
+                    textAnswer = 'пятьсот';                        
+                    break;
+                case 400:
+                    textAnswer = 'четыриста';                        
+                    break;
+                case 300:
+                    textAnswer = 'триста';                        
+                    break;
+                case 200:
+                    textAnswer = 'двести';                        
+                    break;
+                case 100:
+                    textAnswer = 'сто';                        
+                    break;                   
+            }
+        }  else if (remOfNumber2 == 0 && answerNumber < 100){
+                switch (answerNumber) {
+                    case 90:
+                        textAnswer ='девяносто';                        
+                        break;
+                    case 80:
+                        textAnswer ='восемьдесят';                          
+                        break;
+                    case 70:
+                        textAnswer ='семьдесят';                   
+                        break;
+                    case 60:
+                        textAnswer ='шестьдесят';                 
+                        break;
+                    case 50:
+                        textAnswer ='пятьдесят';                      
+                        break;
+                    case 40:
+                        textAnswer ='сорок';                      
+                        break;
+                    case 30:
+                        textAnswer ='тридцать';                      
+                        break;
+                    case 20:
+                        textAnswer ='двадцать';                    
+                        break;
+                    case 10:
+                        textAnswer ='десять';                     
+                        break;                    
+                }  
+        }  else { 
+            hunrds = Math.trunc(answerNumber / 100);
+            tens =  Math.trunc(remOfNumber1 / 10);
+            ones = remOfNumber2;
+            console.log (hunrds,tens,ones);
+
+            if (hunrds > 0) {
+                switch (hunrds) {
+                    case 0:
+                    textAnswer = '';
+                    break;
+                    case 1:
+                        textAnswer = 'сто';
+                        break;
+                    case 2:
+                        textAnswer = 'двести';
+                        break;
+                    case 3:
+                        textAnswer = 'триста';
+                        break;
+                    case 4:
+                        textAnswer = 'четыриста';
+                        break;
+                    case 5:
+                        textAnswer = 'пятьсот';
+                        break;
+                    case 6:
+                        textAnswer = 'шестьсот';
+                        break;
+                    case 7:
+                        textAnswer = 'семьсот';
+                        break;
+                    case 8:
+                        textAnswer = 'восемьсот';
+                        break;
+                    case 9:
+                        textAnswer = 'девятьсот';
+                        break;                
+                }
+            } else {
+                textAnswer = '';  
+            }
+           if (remOfNumber1 < 20 && remOfNumber1 > 9) {
+                switch (remOfNumber1) {
+                    case 10:
+                        textAnswer = textAnswer + ' '+'десять';
+                        break;
+                    case 11:
+                        textAnswer = textAnswer + ' '+'одиннадцать';
+                        break;
+                    case 12:
+                        textAnswer = textAnswer +' '+'двенадцать';
+                        break;
+                    case 13:
+                        textAnswer = textAnswer +' '+'тренадцать';
+                        break;
+                    case 14:
+                        textAnswer = textAnswer +' '+'четырнадцать';
+                        break;
+                    case 15:
+                        textAnswer = textAnswer +' '+'пятнадцать';
+                        break;
+                    case 16:
+                        textAnswer = textAnswer +' '+'шестнадцать';
+                        break;
+                    case 17:
+                        textAnswer = textAnswer +' '+'семнадцать';
+                        break;
+                    case 18:
+                        textAnswer = textAnswer +' '+'восемнадцать';
+                        break;
+                    case 19:
+                        textAnswer = textAnswer +' '+'девятнадцать';
+                        break;                
+                } 
+           } else {
+                switch (tens) {                    
+                    case 1:
+                        textAnswer = '';
+                        break;
+                    case 2:
+                        textAnswer = textAnswer +' '+'двадцать';
+                        break;
+                    case 3:
+                        textAnswer = textAnswer +' '+'тридцать';
+                        break;
+                    case 4:
+                        textAnswer = textAnswer +' '+'сорок';
+                        break;
+                    case 5:
+                        textAnswer = textAnswer +' '+'патьдесят';
+                        break;
+                    case 6:
+                        textAnswer = textAnswer +' '+'шестьдесят';
+                        break;
+                    case 7:
+                        textAnswer = textAnswer +' '+'семьдесят';
+                        break;
+                    case 8:
+                        textAnswer = textAnswer +' '+'восемьдесят';
+                        break;
+                    case 9:
+                        textAnswer = textAnswer +' '+'девяносто';
+                        break;                
+                }
+
+                switch (ones) {
+                    case 0:
+                        textAnswer =  textAnswer + '';
+                        break;
+                    case 1:
+                        textAnswer = textAnswer +' '+'один';
+                        break;
+                    case 2:
+                        textAnswer = textAnswer +' '+'два';
+                        break;
+                    case 3:
+                        textAnswer = textAnswer +' '+'три';
+                        break;
+                    case 4:
+                        textAnswer = textAnswer +' '+'четыре';
+                        break;
+                    case 5:
+                        textAnswer = textAnswer +' '+'пять';
+                        break;
+                    case 6:
+                        textAnswer = textAnswer +' '+'шесть';
+                        break;
+                    case 7:
+                        textAnswer = textAnswer +' '+'семь';
+                        break;
+                    case 8:
+                        textAnswer = textAnswer +' '+'восемь';
+                        break;
+                    case 9:
+                        textAnswer = textAnswer +' '+'девять';
+                        break;                
+                }
+            }
+           
+        }
+        console.log(textAnswer);
+        if (isMinus){
+            return 'минус' + ' ' + textAnswer;
+        }
+        else {
+            return textAnswer;
+        } 
+
+    } else {
+        console.log ('answerNumber is not integer');
     } 
+    
 }
